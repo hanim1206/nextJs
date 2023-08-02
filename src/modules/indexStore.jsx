c55f95c1c2624d7e495297376b91d2a4a13af13e
hanim <hanimkimm@gmail.com>
2023-08-02 19:56:54 +0900

    ss
    dd

---
+++ src/modules/indexStore.jsx
@@ -0,0 +1,10 @@
+import numberStore from './numberStore';
+
+const indexStore = () => ({ 
+    numberStore,
+    // store1,
+    // store2,
+    // ...
+});
+
+export default indexStore;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,14 +2,29 @@ import React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 import Image from 'next/image'
+import { useObserver } from 'mobx-react';
+import indexStore from '../modules/indexStore';
+
 type Props = {}
 
 export default function About({ }: Props) { 
-    return (
-        <>
-            <div>
-                About 페이지입니다
-            </div>
-        </>
+    const { numberStore }  = indexStore();
+
+    const onClickIncrease = () => {
+        numberStore.increaseAction(3);
+    }
+
+    const onClickDecrease = () => {
+        numberStore.decreaseAction(2);
+    }
+
+    return useObserver(() => (
+        <div>
+            <p>현재 값: {numberStore.num}</p>
+
+            <button onClick={onClickIncrease}>증가</button>
+            <button onClick={onClickDecrease}>감소</button>
+        </div>
+    )
     )
 }
