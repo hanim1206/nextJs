2d09e82e2653fc9da3d1c53f83e684f0038c6cdc
hanim <hanimkimm@gmail.com>
2023-08-04 19:28:39 +0900

    ddd
    ddd

--- src/modules/indexStore.jsx
+++ src/modules/indexStore.jsx
@@ -1,7 +1,9 @@
 import numberStore from './numberStore';
+import bgModeStore from './bgModeStore';
 
 const indexStore = () => ({ 
     numberStore,
+    bgModeStore,
     // store1,
     // store2,
     // ...
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { useEffect , useState } from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
 import { useObserver } from 'mobx-react';
@@ -13,17 +14,30 @@ export default function About({ }: Props) {
     const onClickIncrease = () => {
         numberStore.increaseAction(3);
     }
-
     const onClickDecrease = () => {
         numberStore.decreaseAction(2);
     }
+    const ChangeMode = styled.div`width: 300px;height: 40px;display: flex;justify-content: center;align-items: center;border: solid 1px blue;border-radius: 10px;overflow: hidden;
+        select{width:100%;height: 100%;padding: 0 10px;}
+    `;
 
-    return useObserver(() => (
+    const [mode, setMode] = useState('lightMode');
+    console.log(mode);
+
+    return useObserver(() => ( 
         <div>
             <p>현재 값: {numberStore.num}</p>
-
             <button onClick={onClickIncrease}>증가</button>
             <button onClick={onClickDecrease}>감소</button>
+
+            <ChangeMode className='changeMode'>
+                <select onChange={(e) => {
+                    setMode(e.target.value)
+                }}>
+                    <option value="lightMode">Light Mode</option>
+                    <option value="darkMode">Dark Mode</option>
+                </select>
+            </ChangeMode>
         </div>
     )
     )
